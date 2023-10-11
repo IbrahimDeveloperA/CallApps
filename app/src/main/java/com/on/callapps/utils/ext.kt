7f6fc659e301ff956f5080a3e040b21b268ee664
king <ibrahim.kasymov@gmail.com>
2023-10-11 20:09:13 +0600

    +

--- app/src/main/java/com/on/callapps/utils/ext.kt
+++ app/src/main/java/com/on/callapps/utils/ext.kt
@@ -1,7 +1,24 @@
 package com.on.callapps.utils
 
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.Window
 import android.widget.ImageView
+import androidx.viewbinding.ViewBinding
 
-fun ImageView.loadImage(){
-
+fun <T : ViewBinding> Context.createDialog(
+    inflate: (LayoutInflater, ViewGroup?, Boolean) -> T,
+): Pair<T, Dialog> {
+    val inflater: LayoutInflater = LayoutInflater.from(this)
+    val binding = inflate.invoke(inflater, null, false)
+    val dialog = Dialog(this)
+    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+    dialog.setContentView(binding.root)
+    dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    dialog.show()
+    return Pair(binding, dialog)
 }
