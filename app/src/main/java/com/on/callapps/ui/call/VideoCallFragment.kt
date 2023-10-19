83232b642543d465b6861e80815451fa92dba6bf
Abdul-Aziz Kurbanov <abdulazizkurbanov64@gmail.com>
2023-10-19 19:11:27 +0600

    +

--- app/src/main/java/com/on/callapps/ui/call/VideoCallFragment.kt
+++ app/src/main/java/com/on/callapps/ui/call/VideoCallFragment.kt
@@ -1,5 +1,10 @@
 package com.on.callapps.ui.call
 
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Insets.add
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +12,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.on.callapps.databinding.FragmentVideoCallBinding
 
+
 class VideoCallFragment : Fragment() {
 
     private lateinit var binding: FragmentVideoCallBinding
+
 
 
     override fun onCreateView(
